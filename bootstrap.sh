495214ad57db3decc31d1d1f5d8ef12c1770b91a
Ari Palo <ari@aripalo.fi>
2014-09-16 08:36:26 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Configure Sublime command line tool
-# https://www.sublimetext.com/docs/2/osx_command_line.html
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
-
 # Define paths
 SUBLIME_DIR=~/Library/Application\ Support/Sublime\ Text\ 2
 TEMP_DIR=${TMPDIR}SublimeTextConfig
@@ -15,7 +11,7 @@ rm -rf "${TEMP_DIR}"
 git clone --recursive https://github.com/aripalo/SublimeTextConfig.git "${TEMP_DIR}"
 
 # Start (and then shutdown) Sublime Text so we make sure all required folders (mainly ${SUBLIME_DIR} exists
-~/bin/subl --background
+"/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" --background
 pkill Sublime
 
 # Make sure Packages/User does not exits
@@ -31,4 +27,8 @@ ${DOTGLOB_WAS_OFF} && shopt -u dotglob
 rm -rf "${TEMP_DIR}"
 
 # Start Sublime Text so Package Manager will start to install plugins defined in User/Package Control.sublime-settings
-~/bin/subl
+"/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+
+# Configure Sublime command line tool
+# https://www.sublimetext.com/docs/2/osx_command_line.html
+ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
